7e5ce806bc007b67c1ceee28577cce2f53633b97
zahraa fattal <fattal.zahraa@gmail.com>
2024-02-17 23:14:37 +0200

    t

---
+++ src/components/landingDetails.js
@@ -0,0 +1,46 @@
+import React from "react";
+const Details=()=>{
+return(
+
+<div>
+			<div class="container bootdey">
+<div class="col-md-12">
+<section class="panel">
+      <div class="panel-body">
+          <div class="col-md-6">
+              <div class="pro-img-details">
+                  <img src="https://www.bootdey.com/image/550x380/FFB6C1/000000" alt=""/>
+              </div>
+             
+          </div>
+          <div class="col-md-6">
+              <h4 class="pro-d-title">
+                  <a href="#/" class="">
+                   Lorem ipsum dolor
+                  </a>
+              </h4>
+              <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur autem numquam repudiandae laborum corporis accusantium dolore, fuga ipsa nesciunt voluptatibus repellendus, aliquam aperiam quae quibusdam mollitia quaerat. Nam, ratione quibusdam.
+              </p>
+              <div class="product_meta">
+                  <span class="posted_in"> <strong>Categories:</strong> <a rel="tag" href="#/">Jackets</a>, <a rel="tag" href="#/">Men</a>, <a rel="tag" href="#/">Shirts</a>, <a rel="tag" href="#/">T-shirt</a>.</span>
+                  <span class="tagged_as"><strong>Tags:</strong> <a rel="tag" href="#/">mens</a>, <a rel="tag" href="#/">womens</a>.</span>
+              </div>
+              <div class="m-bot15"> <strong>Price : </strong> <span class="amount-old">$544</span>  <span class="pro-price"> $300.00</span></div>
+              <div class="form-group">
+                  <label>Quantity</label>
+                  <input type="quantiy" placeholder="1" class="form-control quantity"/>
+              </div>
+              <p>
+                  <button class="btn btn-round btn-danger" type="button"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
+              </p>
+          </div>
+      </div>
+  </section>
+  </div>
+  </div>
+		</div>
+
+
+)}
+export default Details;
